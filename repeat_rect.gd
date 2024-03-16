13b2b29dc14073f893c65803c6c7fc22e9bda2bf
SirLich <lrkoehler77@gmail.com>
2024-03-16 20:14:16 +0100

    Add filtering selection for images

---
+++ repeat_rect.gd
@@ -0,0 +1,5 @@
+extends Control
+
+func _process(delta: float) -> void:
+	queue_redraw()
+	
--- texture_preview.gd
+++ texture_preview.gd
@@ -1,15 +1,23 @@
 extends TextureRect
+class_name ImagePreview
 
 var SCALE_FACTOR = 0.1
 
-
-func configure(tex : Image):
-	texture = ImageTexture.create_from_image(tex)
-	pivot_offset = size/2
+var _tex : Image
+var tex_scale = Vector2(1,1)
+var original_size : Vector2i
 	
-func _input(event: InputEvent) -> void:
-	if event.is_action("scroll_up"):
-		scale = scale * (1 + SCALE_FACTOR)
-	elif event.is_action("scroll_down"):
-		scale = scale * (1 - SCALE_FACTOR)
-	pivot_offset = size/2
+func configure(image: Image) -> void:
+	_tex = image
+	original_size = image.get_size()
+	set_and_resize_texture()
+
+func set_and_resize_texture():
+	if _tex:
+		print(tex_scale)
+		var new_size = Vector2i(Vector2(original_size) * tex_scale)
+		var new_tex = _tex.duplicate()
+		new_tex.resize(new_size.x, new_size.y, Image.INTERPOLATE_BILINEAR)
+
+		texture = ImageTexture.create_from_image(new_tex)
+		pivot_offset = size/2
