537abe87cbda9e45ad72f9285a1e70ae4230192f
SirLich <lrkoehler77@gmail.com>
2024-03-19 23:38:07 +0100

    Add most of file tree

--- addons/gd_explorer/file_tree.gd
+++ addons/gd_explorer/file_tree.gd
@@ -16,26 +16,33 @@ func _ready() -> void:
 func _on_project_root_set(project_root):
 	_project_root = project_root
 	current_root = _project_root._duplicate()
-	build_tree()
-
-func build_tree():
-	clear()
-	root = create_item()
-	for child in current_root.get_children():
-		print("func build_tree")
-		print(child.get_local())
-		var item = root.create_child()
-		item.set_text(0, child.name)
-		if child.file_exists():
-			item.set_icon(0, file_icon)
-		elif child.directory_exists():
-			item.set_icon(0, folder_icon)
-		else:
-			item.set_icon(0, error_icon)
-		item.set_icon_max_width(0, 24)
-		item.set_metadata(0, child)
 	
+	root = create_item()
+	build_tree_recursive(root, current_root, 0)
+
+var supress_action
+func build_tree_recursive(item : TreeItem, path : FilePath, depth : int):
+	supress_action = true
+				
+	if depth > 3:
+		return
+	for child_path in path.get_children():
+		var child_item = item.create_child()
+		child_item.set_text(0, child_path.name)
+		if child_path.file_exists():
+			child_item.set_icon(0, file_icon)
+		elif child_path.directory_exists():
+			child_item.set_icon(0, folder_icon)
+			var dummy = child_item.create_child()
+			dummy.set_meta("dummy", true)
+			dummy.set_text(0, "...")
+			child_item.collapsed = true
+		else:
+			child_item.set_icon(0, error_icon)
+		child_item.set_icon_max_width(0, 24)
+		child_item.set_metadata(0, child_path)
 		
+	supress_action = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
@@ -48,19 +55,19 @@ func item_selected(item : TreeItem):
 	var filepath : FilePath = item.get_metadata(0)
 	if filepath.is_directory():
 		current_root = filepath
-		build_tree()
+		#build_tree()
 	else:
 		Bus.file_selected.emit(filepath)
 
 
 func _on_up_button_pressed() -> void:
 	current_root = current_root.parent
-	build_tree()
+	#build_tree()
 
 
 func _on_root_button_pressed() -> void:
 	current_root = _project_root._duplicate()
-	build_tree()
+	#build_tree()
 	
 
 func _on_line_edit_text_changed(new_text: String) -> void:
@@ -85,6 +92,16 @@ func _on_line_edit_text_submitted(new_text: String) -> void:
 	if next_valid:
 		item_selected(next_valid)
 
-
 func _on_color_picker_button_color_changed(color: Color) -> void:
 	pass # Replace with function body.
+
+func _on_item_collapsed(item: TreeItem) -> void:
+	if supress_action:
+		return
+		
+	var is_open_action = item.collapsed
+	if item.get_child(0).has_meta("dummy"):
+		item.get_child(0).free()
+		
+	build_tree_recursive(item, item.get_metadata(0), 0)
+		
